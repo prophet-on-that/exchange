c421fcd6239a10c4bf9c59d15be5b56de6f146db
Alex Bates <alex.bates@otastech.com>
2015-08-20 14:37:27 +0000

    Completed bid and ask book-modifying functions.

--- src/Exchange.hs
+++ src/Exchange.hs
@@ -1,41 +1,73 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Exchange where
 
 import STMContainers.Map (Map)
 import qualified STMContainers.Map as Map
 import qualified Focus
 import Control.Concurrent.STM
+import Control.Lens
 
 type Price = Integer
 
-data Offer = Offer
-  { clientId :: !Integer
-  , offerId :: !Integer
-  , count :: !Integer
+data Quote = Quote
+  { _clientId :: !Integer
+  , _offerId :: !Integer
+  , _count :: !Integer
   }
 
-data Offers = Offers
-  { bids :: ![Offer]
-  , asks :: ![Offer]
+makeLenses ''Quote
+
+data Quotes = Quotes
+  { _bids :: ![Quote]
+  , _offers :: ![Quote]
   } 
 
+makeLenses ''Quotes
+
+emptyQuotes :: Quotes
+emptyQuotes
+  = Quotes [] []
+
 data Exchange = Exchange
-  { book :: !(Map Price Offers)
+  { _book :: Map Price Quotes
+  , _bestBid :: TVar Price -- ^ The best bid is derived from the book, we store it here to avoid needless recomputation.
+  , _bestOffer :: TVar Price -- ^ The best offer is derived from the book, we store it here to avoid needless recomputation.
   }
 
--- | Install a bid into the book.
+makeLenses ''Exchange
+
+-- | Install a bid into the book. Bids will never exceed the best offer.
 bid
-  :: Offer
+  :: Quote
+  -> Price -- ^ The maximum price the buyer is willing to meet. 
+  -> Exchange
+  -> STM ()
+bid quote price exch 
+  = Map.focus (Focus.alterM alter) price (view book exch)
+  where
+    alter :: Maybe Quotes -> STM (Maybe Quotes)
+    alter Nothing 
+      = return . Just $ emptyQuotes
+          & ( bids %~ (quote :) )
+    alter (Just quotes)
+      = return . Just $ quotes 
+          & ( bids %~ (++ [quote]) )
+  
+-- | Install an offer into the book.
+offer
+  :: Quote
   -> Price
   -> Exchange
   -> STM ()
-bid offer price exch 
-  = Map.focus (Focus.alterM alter) price (book exch)
+offer quote price exch 
+  = Map.focus (Focus.alterM alter) price (view book exch)
   where
-    alter :: Maybe Offers -> STM (Maybe Offers)
+    alter :: Maybe Quotes -> STM (Maybe Quotes)
     alter Nothing 
-      = return . Just $ Offers [offer] []
-    alter (Just (Offers bids' asks'))
-      = return . Just $ Offers (bids' ++ [offer]) asks'
-  
-  
+      = return . Just $ emptyQuotes
+          & ( offers %~ (quote :) )
+    alter (Just quotes)
+      = return . Just $ quotes 
+          & ( offers %~ (++ [quote]) )
   
